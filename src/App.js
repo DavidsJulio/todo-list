3ec46a6a7bd13d8d7c45d9cc7e522bfba0d33860
DavidsJulio <davidsjulio97@gmail.com>
2021-06-01 22:50:28 +0100

    Protecting front pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import { Route, Switch } from "react-router";
+import { Route, Switch, Redirect } from "react-router";
+import { useContext } from "react";
 import Login from "./pages/Login";
 import StartPage from "./pages/StartPage";
 import Layout from "./components/Layout/Layout";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
         <Route path="/" exact>
-          <StartPage />
+          {authCtx.isLoggedIn && <h1>PAGE TO DO</h1>}
+          {!authCtx.isLoggedIn && <StartPage />}
         </Route>
-        <Route path="/login">
-          <Login />
+        {!authCtx.isLoggedIn && (
+          <Route path="/login">
+            <Login />
+          </Route>
+        )}
+        <Route path="*">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
