b1c0fa72a9ae7edd93fd55e2bff12ffe7c7fb622
DavidsJulio <davidsjulio97@gmail.com>
2021-06-03 22:21:34 +0100

    Fetching todos from firebase

--- src/components/Todo/TodoOngoing.js
+++ src/components/Todo/TodoOngoing.js
@@ -42,13 +42,7 @@ function TodoOngoing() {
     <div className={classes.ongoing}>
       <h3>Todo</h3>
       <div className={classes.list}>
-        <ul>
-          {/* <li>Take dogs for a walk</li>
-          <li>Learn React.js</li>
-          <li>Get a Job</li>
-          <li>Buy a book</li> */}
-          {todosList}
-        </ul>
+        <ul>{todosList}</ul>
       </div>
     </div>
   );
